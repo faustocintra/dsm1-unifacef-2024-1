13577297eea993c639238281938c9218e511f31b
César Daniel Rodrigues <97107669+Czar1347@users.noreply.github.com>
2024-02-27 20:37:59 -0300

    (27/02) Primeiro projeto

---
+++ Projeto1/App.js
@@ -0,0 +1,26 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Olá Mundo!!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#00ffff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontWeight: 'Bold',
+    fontSize: 30
+    
+  }
+});
