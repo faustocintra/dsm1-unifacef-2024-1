ffc56a5c7a6b7d3f51318d25c5d5027ece434e6c
Carol <anacarolinaneias@gmail.com>
2024-03-05 20:43:56 -0300

    (05/03) Componentes e estilos

--- Projeto1/App.js
+++ Projeto1/App.js
@@ -1,20 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Card from './src/Card';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+            <Text style={styles.text}>Olá Mundo!</Text>
+            <StatusBar style="auto"/>
+            <Card title="Uni-FACEF">Centro Universitário Municipal de Franca</Card>
+
+            <Student title="Média">
+            <Text>Nota do 1º bimestre: </Text>
+            <Text>Nota do 2º bimestre: </Text>
+            <Text>Média: </Text>
+            </Student>
     </View>
+    
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ffffff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    fontWeight: 'bold',
+    fontSize: 30
+  }
 });
