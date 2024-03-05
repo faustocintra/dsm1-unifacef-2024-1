dbe540374d858c56d926f49c203e211b453501b8
BrenoMOliveira <brenomoliveira03@gmail.com>
2024-03-05 20:45:37 -0300

    Aula 2 [05/03]

--- projeto-01/App.js
+++ projeto-01/App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Card from './src/card'; 
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Hello World!</Text>
       <StatusBar style="auto" />
+      <Card title="Unifacef">Centro Universitario</Card>
     </View>
   );
 }
