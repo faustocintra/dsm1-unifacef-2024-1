389d34e0f9be92ffc2d4db4df1d2da73141c1957
EderJrDev <ederjuninho2003@gmail.com>
2024-03-05 22:17:05 -0300

    feat: create a new Component Card and Students

--- Projeto1/App.js
+++ Projeto1/App.js
@@ -1,24 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Card from './src/Card';
+import Students from './src/Students';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Hello World '-' </Text>
+      <Text style={styles.text}>Média dos estudantes do Uni-FACEF</Text>
       <StatusBar style="auto" />
+      {/* <Card title="Centro Universitário Municipal de Franca" children="Card Content" /> */}
+      <View style={styles.students}>
+        <Students name="Leandro" grade1={5} grade2={10} />
+        <Students name="Leandro" grade1={5} grade2={10} />
+        <Students name="Leandro" grade1={5} grade2={10} />
+        <Students name="Leandro" grade1={5} grade2={10} />
+      </View>
     </View>
   );
 }
 
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#00ffff'  ,
+    padding: 2,
+    backgroundColor: '#00aaff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  students: {
+    gap: 20,
+  },
   text: {
     fontWeight: 'bold',
-    fontSize: 30,
+    fontSize: 48
   }
 });
---
+++ Projeto1/src/Card/style.js
@@ -0,0 +1,36 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  card: {
+    borderWidth: 1,
+    borderRadius: 16,
+    borderColor: '#0000FF',
+    borderBottomWidth: 0,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 2,
+    elevation: 1,
+    marginLeft: 5,
+    marginRight: 5,
+    marginTop: 10,
+    backgroundColor: '#87CEFA',
+  },
+  titleContainer: {
+    padding: 6,
+    borderBottomWidth: 1,
+    borderBottomColor: '#000000',
+    marginBottom: 5,
+  },
+  title: {
+    fontSize: 30,
+    paddingLeft: 5
+  },
+  children: {
+    fontSize: 16,
+    paddingLeft: 5
+  }
+
+});
+
+export default styles
