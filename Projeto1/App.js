673d11a6081de1e20a997dbd23b8a3e61545fd69
César Daniel Rodrigues <97107669+Czar1347@users.noreply.github.com>
2024-03-05 21:00:53 -0300

    (05/03) Componentes e estilos

--- Projeto1/App.js
+++ Projeto1/App.js
@@ -1,12 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import Card from "./src/Card";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Olá Mundo!!</Text>
       <StatusBar style="auto" />
+      <Card title="Uni-FACEF">Centro Universitario Municipal de Franca</Card>
     </View>
   );
 }
@@ -14,13 +15,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#00ffff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#00ffff",
+    alignItems: "center",
+    justifyContent: "center",
   },
   text: {
-    fontWeight: 'Bold',
-    fontSize: 30
-    
-  }
+    fontWeight: "Bold",
+    fontSize: 30,
+  },
 });
