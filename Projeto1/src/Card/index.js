134e6cc2b7ae206e288e5a87aa9dac9f1073d90d
BrenoMOliveira <brenomoliveira03@gmail.com>
2024-03-12 17:45:02 -0300

    Conclusão do Componente Student

---
+++ Projeto1/src/Card/index.js
@@ -0,0 +1,15 @@
+import { View, Text } from "react-native"
+import styles from './style';
+
+const Card = ({ title, children }) => {
+  return (
+    <View style={styles.card}>
+      <View style={styles.titleContainer}>
+        <Text style={styles.title}>{title}</Text>
+      </View>
+      <Text style={styles.children}>{children}</Text>
+    </View>
+  )
+};
+
+export default Card;
---
+++ Projeto1/src/Students/style.js
@@ -0,0 +1,26 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  card: {
+    borderRadius: 16,
+    borderColor: '#ffa500',
+    borderWidth: 2,
+    borderStyle: "solid",
+    backgroundColor: '#ffffe0',
+    padding: 10
+  },
+  titleContainer: {
+    borderBottomWidth: 1,
+    borderBottomColor: '#ffa500',
+    marginBottom: 5
+  },
+  title: {
+    fontSize: 25,
+    fontWeight: 'bold'
+  },
+  subTitle: {
+    fontSize: 20
+  }
+});
+
+export default styles
