eaa7fa9b7ee0079979778f3d0bee9ff5ac04abee
Igor <igorpcampos2004@gmail.com>
2024-03-12 18:20:32 -0300

    atividade Student

--- Projeto1/src/student/index.js
+++ Projeto1/src/student/index.js
@@ -1,12 +1,22 @@
-import React from "react";
-import { Text, View } from "react-native";
-import { styles } from "./style";
+import React from 'react';
+import { Text, View } from 'react-native';
+import styles from './style';
 
-const Student = ({ title, children }) => {
-    return (
-        <View>
-        </View>
-    )
-}
+const Students = ({ name, primeiraNota, segundaNota }) => {
 
-export default Student
+  return (
+
+    <View style={styles.card}>
+      <View style={styles.gradientBackground}>
+      </View>
+      <Text style={styles.cardText}>Aluno: {name}</Text>
+      <View style={styles.context}>
+        <Text style={styles.cardText} > Primeira Nota: {primeiraNota}</Text>
+        <Text style={styles.cardText}> Segunda Nota: {segundaNota}</Text>
+      </View>
+    </View>
+
+  );
+};
+
+export default Students;
