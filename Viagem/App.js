19a19aefde37945bb9f46f8454cc4101959c6ea8
César Daniel Rodrigues <97107669+Czar1347@users.noreply.github.com>
2024-03-19 21:07:20 -0300

    correções

--- Viagem/App.js
+++ Viagem/App.js
@@ -6,6 +6,9 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Abastecimento distancia={100} etanol={1} gasolina={2}></Abastecimento>
+      <Abastecimento distancia={200} etanol={1} gasolina={2}></Abastecimento>
+      <Abastecimento distancia={300} etanol={1} gasolina={2}></Abastecimento>
+      <Abastecimento distancia={400} etanol={1} gasolina={2}></Abastecimento>
     </View>
   );
 }
