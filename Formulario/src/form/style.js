5d276f5921f3782aaebd3886c00bb00f7ee78d7d
Tania maraiza <taniamaraizasouzacampos25@gmail.com>
2024-04-02 20:37:13 -0300

    (02/04) inicio do projeto Formulário

---
+++ Formulario/src/form/style.js
@@ -0,0 +1,14 @@
+import {StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    textInput:{
+        bordeColoe:'#888' ,
+        borderWildh: 1
+    },
+    horizontal: {
+        flexDirection: 'row',
+        justifyContent: 'spaace-beetween',
+        marginBottom: 10
+    }    
+})
+export default styles
