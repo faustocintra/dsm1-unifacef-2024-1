13f765d9f113b6d421515ab355c74e69869763bb
Brenda Araujo <brendapaulin00@hotmail.com>
2024-04-02 21:27:57 -0300

    aula do dia 02-04

--- projeto1/src/Student/index.js
+++ projeto1/src/Student/index.js
@@ -1,21 +1,22 @@
-import React from 'react';
-import { Text, View } from 'react-native';
-import styles from './styles';
+import React from 'react'
+import {Text, View} from 'react-native'
+import styles from './styles'
 
-const Student = ({ name, grade1, grade2 }) => {
-    const media = (grade1 + grade2) / 2;
-
-    return (
-        <View style={styles.students}>
-            <View style={styles.titleContainer}>
-            <Text style={styles.name}>{name}</Text>
-            </View>
-            <Text style={styles.grade}>Nota 1° Bim: </Text>
-            <Text >{grade1}</Text>
-            <Text style={styles.grade}>Nota 2° Bim: {grade2}</Text>
-            <Text style={styles.media}>Média: {media}</Text>
-        </View>
-    );
-};
-
-export default Student
+export default function Student({name, grade1, grade2}) {
+  return <View style={styles.card}>
+    <View style={styles.title}>
+      <Text style={styles.titleText}>{name}</Text>
+    </View>
+    <View>
+      <View style={styles.horizontal}>
+        <Text>Nota do 1º bimestre: </Text><Text style={styles.textBlue}>{grade1}</Text>
+      </View>
+      <View style={styles.horizontal}>
+        <Text>Nota do 2º bimestre: </Text><Text style={styles.textBlue}>{grade2}</Text>
+      </View>
+      <View style={styles.horizontal}>
+        <Text>Média: </Text><Text style={styles.textBlue}>{(grade1 + grade2) / 2}</Text>
+      </View>
+    </View>
+  </View>
+}
