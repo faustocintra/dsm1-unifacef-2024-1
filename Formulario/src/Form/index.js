58c3b7890afac479d91e3617789828a27e6cb713
Roberto-BR29 <robertopapacidero@gmail.com>
2024-04-09 20:11:47 -0300

    (09/04) Formulario sobre area

--- Formulario/src/Form/index.js
+++ Formulario/src/Form/index.js
@@ -7,14 +7,16 @@ export default function Form() {
   const [state, setState] = React.useState({
     base: '',
     altura: '',
-    tipo: ''
+    tipo: '',
+    area: ''
   })
   // Criando as variáveis de estado somente-leitura
   // usando desestruturação
   const {
     base,
     altura,
-    tipo
+    tipo,
+    area
   } = state
 
   // Funções de manipulação de eventos usando o formato arrow function
@@ -27,7 +29,22 @@ export default function Form() {
   }
 
   function handleButtonPress() {
-  
+    let result
+    switch(tipo.toUpperCase()) {
+      case 'R':     // Retângulo
+        result = Number(base) * Number(altura)
+        break
+      case 'T':     // Triângulo
+        result = Number(base) * Number(altura) / 2
+        break
+      case 'E':     // Elipse/círculo
+        result = (Number(base) / 2) * (Number(altura) / 2) * Math.PI
+        break
+      default:      // Forma inválida ou desconhecida
+        result = '[TIPO INVÁLIDO]'
+    }
+    // Atualiza o estado da área com o resultado encontrado
+    setState({...state, area: result})
   }
 
   return (
@@ -38,6 +55,8 @@ export default function Form() {
         <TextInput 
           style={styles.textInput}
           onChangeText={handleBaseChangeText}
+          inputMode='decimal'
+          keyboardType='decimal-pad'
         />
       </View>
       <View style={styles.horizontal}>
@@ -45,6 +64,8 @@ export default function Form() {
         <TextInput 
           style={styles.textInput}
           onChangeText={handleAlturaChangeText}
+          inputMode='decimal'
+          keyboardType='decimal-pad'
         />
       </View>
       <View style={styles.horizontal}>
@@ -52,15 +73,17 @@ export default function Form() {
         <TextInput 
           style={styles.textInput}
           onChangeText={handleTipoChangeText}
+          maxLength={1}
+          autoCapitalize='characters'
         />
       </View>
       <View style={styles.horizontal}>
         <Button title="Calcular" onPress={handleButtonPress} />
       </View>
       <View style={styles.horizontal}>
-        <Text>Área: </Text>
-        <Text>Estado: {JSON.stringify(state)}</Text>
+        <Text>Área: {area}</Text>
       </View>
     </View>
-    )
+  )
 }
+
