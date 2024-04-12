0d410d23b8d0bc8c2fa0007f4463f7e67321123e
César Daniel Rodrigues <97107669+Czar1347@users.noreply.github.com>
2024-04-12 16:43:18 -0300

    Trabalho 1Bimestre

--- trabalho-1bimestre/src/form/style.js
+++ trabalho-1bimestre/src/form/style.js
@@ -2,13 +2,22 @@ import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
     textInput: {
+        width:100,
         borderColor: '#888',
         borderWidth: 1,
+        textAlign:'center'
     },
     horizontal: {
         flexDirection: 'row',
         justifyContent: 'space-between',
-        marginBottom: 10
+        marginBottom: 10,
+        alignItems:'center'
+    },
+    element: {
+        marginRight:5
+    },
+    result:{
+        maxWidth:110
     }
 
 })
