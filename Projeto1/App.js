eadec23ea62ea2628da403aa4f3e90411202c14d
Igor <igorpcampos2004@gmail.com>
2024-04-14 09:56:22 -0300

    trabalho bhaskara

--- Projeto1/App.js
+++ Projeto1/App.js
@@ -3,24 +3,20 @@ import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import Card from './src/card';
 import Student from './src/student/index'
 import Viagem from './src/viagem/index'
+import Bhaskara from './src/Bhaskara/index'
 
 export default function App() {
-  const handlePress = () => {
-    console.log('Botão pressionado!');
-  };
 
   return (
     <View style={styles.container}>
-      <TouchableOpacity onPress={handlePress} style={styles.button}>
-        <Text style={styles.text}>Mobile</Text>
-      </TouchableOpacity>
       <StatusBar style="auto" />
       <Card title="Unifacef">Centro universitario</Card>
-      <Student name={Igor} primeiraNota={9} segundaNota={8} faltas={10}/>
+      {/* <Student name={"Igor"} primeiraNota={9} segundaNota={8} faltas={10}/>
       <Viagem distancia={100} precoEtanol={4.50} precoGasolina={5.00} />
       <Viagem distancia={200} precoEtanol={4.60} precoGasolina={5.10} />
       <Viagem distancia={150} precoEtanol={4.70} precoGasolina={5.20} />
-      <Viagem distancia={180} precoEtanol={4.80} precoGasolina={5.30} />
+      <Viagem distancia={180} precoEtanol={4.80} precoGasolina={5.30} /> */}
+      <Bhaskara/>
     </View>
   );
 }
