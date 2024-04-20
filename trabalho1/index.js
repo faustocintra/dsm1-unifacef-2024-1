0af08b7a9a1806bfe640dc008571c6865ee4d6c4
Codebit-Brenda <brenda@codebit.com.br>
2024-04-20 18:42:03 -0300

    trabalho 1 - brenda-paulino-de-araujo

---
+++ trabalho1/index.js
@@ -0,0 +1,4 @@
+// index.js
+import React from 'react';
+import App from './App';
+
---
+++ trabalho1/styles.js
@@ -0,0 +1,21 @@
+// styles.js
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    padding: 20,
+  },
+  input: {
+    width: '20%',
+    height: 60,
+    borderColor: 'blue',
+    borderWidth: 2,
+    marginBottom: 10,
+    paddingHorizontal: 10,
+  },
+  resultContainer: {
+    marginTop: 50,
+  },
+});
