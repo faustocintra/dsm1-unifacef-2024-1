9c42aeebd1062b5c686a407650be33d33cd5d90a
ThiagoCorreiaBR2 <136650726+ThiagoCorreiaBR2@users.noreply.github.com>
2024-05-14 20:35:46 -0300

    Update index.js

--- NavCurso/src/SobreMim/index.js
+++ NavCurso/src/SobreMim/index.js
@@ -10,8 +10,8 @@ export default function Curso({navigation}) {
                      <Image source={{ uri: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/85/Smiley.svg/640px-Smiley.svg.png' }} style={{ width: 200, height: 200 }} />
                 </View>
                 <Text style={styles.texto}>Nome: Thiago Correia Azarias</Text>
-                <Text style={styles.texto}>Telefone: 16993641082</Text>
-                <Text style={styles.texto}>E-mail: thiagoazarias.oficial@gmail.com</Text>
+                <Text style={styles.texto}>Telefone: 16993640000</Text>
+                <Text style={styles.texto}>E-mail: thiagoazarias.placeholder@gmail.com</Text>
                 <Pressable onPress={() => 
                     navigation.navigate('Meu App')}>
                     <Text style={styles.button}>Voltar</Text>
