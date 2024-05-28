1c4e13d852c8f91207c16c3d62656e4da5fa126f
Mariana E Reis Alves <marianaalvestrabalhos@gmail.com>
2024-05-28 20:15:49 -0300

    Tentando fazer o código do instagram

---
+++ Instagram/src/instagram/styles.js
@@ -0,0 +1,67 @@
+// styles.js
+
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#fafafa',
+        backgroundImage: 'linear-gradient(to bottom, #aaaaaa, #ffffff)', // Gradiente de cima para baixo do cinza ao branco
+      },
+  language: {
+    marginBottom: 60,
+  },
+  logo: {
+    width: 100,
+    height: 100,
+    marginBottom: 60,
+  },
+  formContainer: {
+    width: '80%',
+  },
+  input: {
+    width: '100%',
+    height: 40,
+    backgroundColor: '#e0e0e0',
+    borderRadius: 5,
+    paddingHorizontal: 10,
+    marginBottom: 10,
+  },
+  button: {
+    width: '100%',
+    height: 40,
+    backgroundColor: '#3897f0',
+    borderRadius: 30,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 10,
+    overflow: 'hidden',
+  },
+  buttonText: {
+    color: '#fff',
+    fontWeight: 'bold',
+  },
+  forgotPassword: {
+    marginTop: 10,
+    color: '#003569',
+    textAlign: 'center',
+  },
+  createAccountButton: {
+    width: '100%',
+    height: 40,
+    borderRadius: 30,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 100,
+    borderWidth: 1,
+    overflow: 'hidden',
+  },
+  createAccountButtonText: {
+    color: '#003569',
+    fontWeight: 'bold',
+  },
+});
+
+export default styles;
