af7dea845e5f888bc3191df97a2e8e47667d106c
Vinicius Cintra Ferreira <vinicintra3101@outlook.com>
2024-06-01 22:54:32 -0300

    Login-Completo

---
+++ Instagram/src/Home/index.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Text, View, StyleSheet, Image } from "react-native";
+
+const HomePage = ({ }) => {
+  return (
+    <View style={styles.container}>
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 30,
+    color: '#000',
+  },
+});
+
+export default HomePage
