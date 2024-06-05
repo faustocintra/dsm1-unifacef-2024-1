84d248d942946fdca4aee0a521adf6d966690690
César Daniel Rodrigues <97107669+Czar1347@users.noreply.github.com>
2024-06-05 14:18:25 -0300

    Refatorando parte do storys para melhorar manuteção

--- Instagram/src/telaInicial/index.jsx
+++ Instagram/src/telaInicial/index.jsx
@@ -5,41 +5,7 @@ import { header, story } from "./style"
 import { Image } from "expo-image"
 import { Entypo } from "@expo/vector-icons"
 import { TabActions } from "@react-navigation/native"
-
-const storys = [
-  {
-    id: 1,
-    name: "Seu story",
-    image: require("../../assets/users/user_1.jpg"),
-  },
-  {
-    id: 0,
-    name: "User 1",
-    image: require("../../assets/users/user_2.jpg"),
-  },
-  {
-    id: 0,
-    name: "User 2",
-    image: require("../../assets/users/user_3.jpg"),
-  },
-  {
-    id: 0,
-    name: "User 3",
-    image: require("../../assets/users/user_4.jpg"),
-  },
-  ,
-  {
-    id: 0,
-    name: "User 4",
-    image: require("../../assets/users/user_5.jpg"),
-  },
-  ,
-  {
-    id: 0,
-    name: "User 5",
-    image: require("../../assets/users/user_6.jpg"),
-  },
-]
+import Storys from "../components/storys"
 
 export default function TelaInicial({ navigation }) {
   return (
@@ -56,31 +22,7 @@ export default function TelaInicial({ navigation }) {
           </View>
         </View>
       </View>
-      <ScrollView horizontal={true} showsHorizontalScrollIndicator={false} style={story.scroll}>
-        {storys.map((data) => {
-          return (
-            <TouchableOpacity style={{ borderWidth: 1, borderColor: "#c13584", height: "20%" }}>
-              <View style={story.storyBlock}>
-                {data.id == 1 ? (
-                  <View style={story.entypoView}>
-                    <Entypo name="circle-with-plus" style={story.entypo} />
-                  </View>
-                ) : null}
-                {data.id == 1 ? (
-                  <View style={story.viewStoryCircleProfile}>
-                    <Image source={data.image} style={story.imageStoryCircle} />
-                  </View>
-                ) : (
-                  <View style={story.viewStoryCircle}>
-                    <Image source={data.image} style={story.imageStoryCircle} />
-                  </View>
-                )}
-                <Text style={{ textAlign: "center", fontSize: 10, marginTop: 5, fontWeight: 600 }}>{data.name}</Text>
-              </View>
-            </TouchableOpacity>
-          )
-        })}
-      </ScrollView>
+      <Storys />
     </View>
   )
 }
