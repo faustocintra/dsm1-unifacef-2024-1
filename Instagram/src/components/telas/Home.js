c96882bd95d58e8f085bab997fa960d454458e5b
nico <nicolastch04@gmail.com>
2024-06-06 13:13:49 -0300

    Posts, Stories e DB

--- Instagram/src/components/telas/Home.js
+++ Instagram/src/components/telas/Home.js
@@ -1,26 +1,51 @@
-import { View, Text, StatusBar, ScrollView } from 'react-native'
-import React from 'react'
-import FontAwesome from 'react-native-vector-icons/FontAwesome'
-import Feather from 'react-native-vector-icons/Feather'
-import Stories from '../telasComponents/Stories'
+import React from 'react';
+import {View, Text, StatusBar, ScrollView} from 'react-native';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import Feather from 'react-native-vector-icons/Feather';
+import Ionic from 'react-native-vector-icons/Ionicons';
+import Stories from '../telasComponents/Stories';
+import Post from '../telasComponents/Post';
 
 const Home = () => {
   return (
-    <View style={{backgroundColor: 'white', height: '100% '}}>
-      <StatusBar backgroundColor="white" barStyle="dark-content" animated={true}></StatusBar>
-      <View style={{ justifyContent: 'space-between', flexDirection:'row', paddingHorizontal: 15, alignItems: 'center'}}>
-        <FontAwesome name="plus-square-o" style={{fontSize: 24}}></FontAwesome>
-        <Text style={{fontFamily: "Lobster-Regular", fontSize: 25, fontWeight:'500'}}>
-            Instagram
+    <View style={{backgroundColor: 'white', height: '100%'}}>
+      <StatusBar
+        backgroundColor="white"
+        barStyle="dark-content"
+        animated={true}
+      />
+      <View
+        style={{
+          justifyContent: 'space-between',
+          flexDirection: 'row',
+          paddingHorizontal: 15,
+          alignItems: 'center',
+        }}>
+        <FontAwesome name="plus-square-o" style={{fontSize: 24}} />
+        <Text
+          style={{
+            fontFamily: 'Lobster-Regular',
+            fontSize: 25,
+            fontWeight: '500',
+          }}>
+          Instagram
         </Text>
-        <Feather name="navigation" style={{fontSize: 24}}/>
+        <Feather name="navigation" style={{fontSize: 24}} />
       </View>
 
       <ScrollView>
-
+        <Stories />
+        <Post />
+        <View
+          style={{justifyContent: 'center', alignItems: 'center', padding: 20}}>
+          <Ionic
+            name="ios-reload-circle-sharp"
+            style={{fontSize: 60, opacity: 0.2}}
+          />
+        </View>
       </ScrollView>
     </View>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
