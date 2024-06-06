34349efbc4a2013a70d38658ca41944ef4064697
ThiagoCorreiaBR2 <thiagogithub23@gmail.com>
2024-06-06 20:27:03 -0300

    styles

---
+++ Instagram/src/Login/styles.js
@@ -0,0 +1,38 @@
+import { StyleSheet } from 'react-native'
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  texto:{
+    padding:10
+  },
+  button:{
+    padding:5,
+    margin:5,
+    backgroundColorcolor:"blue",
+    color: 'blue',
+    border: '2px solid blue',
+  },
+  input:{
+    textAlign: 'center',
+    padding:5,
+    margin:5,
+    width: 200,
+    borderRadius: 10,
+    selectionColor: 'blue',
+    backgroundColor:"lightgray",
+    color: 'black',
+    border: '2px solid black',
+  },
+  inputstorage:{
+    paddingTop:100
+  },
+  icon:{
+    color:"black"
+  }
+})
+
+export default styles
