1cb21553e7a2efeb6332b2158c6ec7bd74dbfc6b
Leandro1105 <leandradevieira38@gmail.com>
2024-06-10 22:46:58 -0300

    Alteração no APP JS

--- Instagram/App.js
+++ Instagram/App.js
@@ -1,12 +1,9 @@
-import { StatusBar } from "expo-status-bar";
-
 import StackRoutes from "./src/routes/stack.routes";
 
 export default function App() {
   return (
     <>
       <StackRoutes />
-      <StatusBar style="auto" />
     </>
   );
 }
