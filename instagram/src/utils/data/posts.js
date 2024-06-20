a299348c1f7176942f3cbe751e23e07df2af98ca
DaniloNogueira-Silva <dansilva730x@gmail.com>
2024-06-20 19:59:10 -0300

    Entrega prova 2° bimestre

---
+++ instagram/src/utils/data/posts.js
@@ -0,0 +1,37 @@
+export const POSTS = [
+  {
+    id: '1',
+    user: 'Alice Green',
+    userImage: 'https://source.unsplash.com/random/400x400/?female',
+    postImage: 'https://source.unsplash.com/random/600x600/?female',
+    description: 'A serene morning by the lake.',
+  },
+  {
+    id: '2',
+    user: 'Bob White',
+    userImage: 'https://source.unsplash.com/random/400x400/?male',
+    postImage: 'https://unsplash.com/photos/tn2rBnvIl9I',
+    description: 'Exploring the ancient ruins!',
+  },
+  {
+    id: '3',
+    user: 'Carol Black',
+    userImage: 'https://source.unsplash.com/random/400x400/?woman',
+    postImage: 'https://unsplash.com/photos/3ZUsNJhi_Ik',
+    description: 'Captured this beautiful sunset at the beach.',
+  },
+  {
+    id: '4',
+    user: 'David Lee',
+    userImage: 'https://source.unsplash.com/random/400x400/?man',
+    postImage: 'https://unsplash.com/photos/YApiWyp0lqo',
+    description: 'Enjoying a day in the snowy mountains.',
+  },
+  {
+    id: '5',
+    user: 'Eva Grey',
+    userImage: 'https://source.unsplash.com/random/400x400/?portrait',
+    postImage: 'https://unsplash.com/photos/pHCdS7Ehp08',
+    description: 'Relaxing weekend at the countryside.',
+  },
+];
---
+++ instagram/src/utils/data/stories.js
@@ -0,0 +1,26 @@
+export const STORIES = [
+  {
+    id: '1',
+    name: 'Alice Green',
+    image: 'https://source.unsplash.com/random/400x400/?female',
+  },
+  {
+    id: '2',
+    name: 'Bob White',
+    image: 'https://source.unsplash.com/random/400x400/?male',
+  },
+  {
+    id: '3',
+    name: 'Carol Black',
+    image: 'https://source.unsplash.com/random/400x400/?woman',
+  },
+  {
+    id: '4',
+    name: 'David Lee',
+    image: 'https://source.unsplash.com/random/400x400/?man',
+  }, {
+    id: '5',
+    name: 'Eva Grey',
+    image: 'https://source.unsplash.com/random/400x400/?portrait',
+  },
+];
